e4e162b31cf935208470e9763aadbdbb57f6822a
Nazmul Huda <nznazmulhuda04@gmail.com>
2024-10-27 01:31:21 +0600

    project init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+	return (
+		<>
+			<div className="w-full">
+				<h1 className="text-center text-green-600 text-xl text-wrap">
+					This is a mangement website for orbit outfit.
+				</h1>
+			</div>
+		</>
+	);
+}
+
+export default App;
