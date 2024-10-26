acd9ba27006f21b02d39c6370a4e6aa026678e39
Nazmul Huda <nznazmulhuda04@gmail.com>
2024-10-27 02:14:32 +0600

    install shadecn package and setup dark mode [DONE]

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import { Button } from "./components/ui/button";
+
 function App() {
 	return (
 		<>
 			<div className="w-full">
-				<h1 className="text-center text-green-600 text-xl text-wrap">
+				<h1 className="text-center text-green-600 text-xl text-wrap dark:text-red-50">
 					This is a mangement website for orbit outfit.
 				</h1>
+
+				<Button>Click me</Button>
 			</div>
 		</>
 	);
